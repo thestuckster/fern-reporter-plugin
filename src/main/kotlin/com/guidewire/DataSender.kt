e5fe3cefc7509def8803a7d948248311734a3ffe
thestuckster <sstucky3@gmail.com>
2025-03-23 22:51:14 -0500

    fix: publish endpoint; handle redirects if they occur

--- src/main/kotlin/com/guidewire/DataSender.kt
+++ src/main/kotlin/com/guidewire/DataSender.kt
@@ -15,23 +15,32 @@ fun sendTestRun(testRun: TestRun, fernUrl: String, verbose: Boolean): Result<Uni
     }
 
     val payload = json.encodeToString(testRun)
-    val endpoint = URI("$fernUrl/api/testrun").normalize()
+    val endpoint = "$fernUrl/api/testrun/"
 
     if (verbose) {
       println("Sending POST request to $endpoint...")
     }
 
-    val client = HttpClient.newBuilder().build()
-    val request = HttpRequest.newBuilder()
-      .uri(endpoint)
-      .header("Content-Type", "application/json")
-      .POST(HttpRequest.BodyPublishers.ofString(payload))
-      .build()
-
-    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-
-    if (response.statusCode() >= 300) {
+    val response = postTestRun(endpoint, fernUrl, payload)
+    if (response.statusCode()>= 300) {
       throw RuntimeException("Unexpected response code: ${response.statusCode()}")
     }
   }
+}
+
+private fun postTestRun(endpoint: String, fernUrl: String, payload: String): HttpResponse<String> {
+  val client = HttpClient.newBuilder().build()
+  val request = HttpRequest.newBuilder()
+    .uri(URI(endpoint).normalize())
+    .header("Content-Type", "application/json")
+    .POST(HttpRequest.BodyPublishers.ofString(payload))
+    .build()
+
+  var response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+  if(response.statusCode() == 307) {
+    val locationHeader = response.headers().firstValue("location")
+    response = postTestRun(fernUrl + locationHeader, fernUrl, payload)
+  }
+  return response
 }
